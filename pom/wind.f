d57528a87612e5636ff6a4e9fcff42c03518d095
RinceWND <RinceWND@yandex.ru>
2018-06-01 16:49:11 +1000

    Fixes to wind reading

--- pom/wind.f
+++ pom/wind.f
@@ -90,153 +90,123 @@
         vwnd_buf_coarse = sf_wi*vwnd_buf_coarse
 
 ! interpolation
-      if(calc_interp) then ! fhx:interp_flag:add flag for interp fgrid.  
+        if ( calc_interp ) then ! fhx:interp_flag:add flag for interp fgrid.  
 
-       do k=1,wn
-        uwnd_coarse = uwnd_buf_coarse( :, :, k )
-        vwnd_coarse = vwnd_buf_coarse( :, :, k )
+          do k=1,wn
+            uwnd_coarse = uwnd_buf_coarse( :, :, k )
+            vwnd_coarse = vwnd_buf_coarse( :, :, k )
 
-        call interp_mask_2d(uwnd_coarse,1,east_e,north_e,uwnd_fine)
-        call interp_mask_2d(vwnd_coarse,2,east_e,north_e,vwnd_fine)
+            call interp_mask_2d(uwnd_coarse,1,east_e,north_e,uwnd_fine)
+            call interp_mask_2d(vwnd_coarse,2,east_e,north_e,vwnd_fine)
 !fhx: after interpolation, i=1,3 and j=1,3 seem to have problems
-      if (n_west.eq.-1) then
+            if (n_west.eq.-1) then
 !         print*, uwnd_fine(1,1),uwnd_fine(1,2),uwnd_fine(2,1)
 !         print*, vwnd_fine(1,1),vwnd_fine(1,2),vwnd_fine(2,1)
 !         print*, ''
-       do i=1,2
-         uwnd_fine(i,:)=uwnd_fine(3,:)      
-         vwnd_fine(i,:)=vwnd_fine(3,:)
-       enddo
+              do i=1,2
+                uwnd_fine(i,:)=uwnd_fine(3,:)      
+                vwnd_fine(i,:)=vwnd_fine(3,:)
+              enddo
+            endif
 
-      endif
+            if(n_south.eq.-1) then
+              do j=1,2
+                uwnd_fine(:,j)=uwnd_fine(:,3)       
+                vwnd_fine(:,j)=vwnd_fine(:,3)        
+              enddo 
+            endif
+            uwnd_buf(:,:,k)=uwnd_fine
+            vwnd_buf(:,:,k)=vwnd_fine
 
-      if(n_south.eq.-1) then
-       do j=1,2
-         uwnd_fine(:,j)=uwnd_fine(:,3)       
-         vwnd_fine(:,j)=vwnd_fine(:,3)        
-       enddo 
-      endif
-         uwnd_buf(:,:,k)=uwnd_fine
-         vwnd_buf(:,:,k)=vwnd_fine
- 
-       enddo ! k=1,wn
+          enddo ! k=1,wn
               
-       else
+        else
 
           uwnd_buf = reshape(uwnd_buf_coarse, shape(uwnd_buf))    
           vwnd_buf = reshape(vwnd_buf_coarse, shape(vwnd_buf))  
   
-       endif !  if(calc_interp) then fhx:interp_flag
+        endif !  if(calc_interp) then fhx:interp_flag
                 
-         do i=1,wn-1
+        do i=1,wn-1
 
-            if (  sec_in_day >= nsec(i) .and.
+          if (  sec_in_day >= nsec(i) .and.
      $            sec_in_day <  nsec(i+1)  ) then
 
             uwnd_a = uwnd_buf( :, :, i )
             vwnd_a = vwnd_buf( :, :, i )
             uwnd_b = uwnd_buf( :, :, i+1 )
             vwnd_b = vwnd_buf( :, :, i+1 )
-           
-               exit
+
+            exit
                
-            endif   
+          endif   
 
-         enddo
+        enddo
          
-         if (  sec_in_day >= nsec(wn) ) then
+        if (  sec_in_day >= nsec(wn) ) then
 
-            uwnd_a = uwnd_buf( :, :, wn )
-            vwnd_a = vwnd_buf( :, :, wn )
-            
-            d_fwd = d_in + 24 * 3600
-            n = int((d_fwd-d_off)/86400.)*4+1
-            n = mod(n, 1460 + 4*inc_leap(d_in%year))
+          uwnd_a = uwnd_buf( :, :, wn )
+          vwnd_a = vwnd_buf( :, :, wn )
 
-!!*      write( infile, '( //trim(windf)//"_",i4.4,2i2.2,".nc" )' )
-!!*  $        d_in%year, d_in%month, d_in%day
-!         write( infile, '( a4,"_",i4.4,2i2.2,".nc" )' )
-!debug     $        windf, d_in%year, d_in%month, d_in%day
-!     $        windf, d_off%year, d_off%month, d_off%day
-            if (calc_mflx) then
-              write( infile, '( a3,".",i4.4,".nc" )' )
-     $        "hfl", d_fwd%year
-            else
-              write( infile, '( a3,".",i4.4,".nc" )' )
-     $        "mfl", d_fwd%year
-            end if
-!!       write( infile, '( "gfsw_",i4.4,2i2.2,".nc" )' )   ! fhx:read gfsw wind
-!!   $        d_in%year, d_in%month, d_in%day
-!            write( infile, '( "gfs_",i4.4,2i2.2,".nc" )' ) ! fhx:read gfs wind
-!     $           d_off%year, d_off%month, d_off%day
+          d_fwd = d_in + 24 * 3600
+          n = int((d_fwd-d_off)/86400.)*4+1
+          n = mod(n, 1460 + 4*inc_leap(d_in%year))
 
-! fhx: check wind data exists,is ~exist, set wind to be 0.10/26/2010             
-         inquire(file='in/'//trim(windf)//'/'//trim(infile),
-     $           exist=lexist)
-!!       inquire(file='in/gfsw/'//trim(infile),exist=lexist)
+          write( infile, '( i4.4,".nc" )' ) d_fwd%year
 
-         if(lexist) then
+          if ( calc_mflx ) then
 
-           if (calc_mflx) then
-             call read_wind_pnetcdfc
+            call read_wind_pnetcdfc
      &         ( uwnd_buf_coarse, vwnd_buf_coarse, trim(infile), n )
-           else
-             call read_mflx_pnetcdf
+
+          else
+
+            call read_mflx_pnetcdf
      $         ( uwnd_buf_coarse, vwnd_buf_coarse, trim(infile), n )
-           end if
 
-           uwnd_buf_coarse = sf_wi*uwnd_buf_coarse
-           vwnd_buf_coarse = sf_wi*vwnd_buf_coarse
+          end if
 
-         else
-              if ( my_task == master_task ) then
-                 write(*,'(/2a)') 
-     $              "missing wind data at wind_init : "
-     $              , trim(infile)
-               endif
-             uwnd_buf_coarse = 0.
-             vwnd_buf_coarse = 0.
-             
-         endif
+          uwnd_buf_coarse = sf_wi*uwnd_buf_coarse
+          vwnd_buf_coarse = sf_wi*vwnd_buf_coarse
+
+        endif
 
 ! interpolation
-      if(calc_interp) then ! fhx:interp_flag:add flag for interp fgrid.  
+        if ( calc_interp ) then ! fhx:interp_flag:add flag for interp fgrid.  
 
-       do k=1,wn
-        uwnd_coarse = uwnd_buf_coarse( :, :, k )
-        vwnd_coarse = vwnd_buf_coarse( :, :, k )
-        call interp_mask_2d(uwnd_coarse,1,east_e,north_e,uwnd_fine)
-        call interp_mask_2d(vwnd_coarse,2,east_e,north_e,vwnd_fine)
+          do k=1,wn
+            uwnd_coarse = uwnd_buf_coarse( :, :, k )
+            vwnd_coarse = vwnd_buf_coarse( :, :, k )
+            call interp_mask_2d(uwnd_coarse,1,east_e,north_e,uwnd_fine)
+            call interp_mask_2d(vwnd_coarse,2,east_e,north_e,vwnd_fine)
 !fhx: after interpolation, i=1,2 and j=1,2 seem to have problems
-      if (n_west.eq.-1) then
-       do i=1,2
-         uwnd_fine(i,:)=uwnd_fine(3,:)
-         vwnd_fine(i,:)=vwnd_fine(3,:)
-       enddo
-      endif
+            if (n_west.eq.-1) then
+              do i=1,2
+                uwnd_fine(i,:)=uwnd_fine(3,:)
+                vwnd_fine(i,:)=vwnd_fine(3,:)
+              enddo
+            endif
 
-      if(n_south.eq.-1) then
-       do j=1,2
-         uwnd_fine(:,j)=uwnd_fine(:,3)
-         vwnd_fine(:,j)=vwnd_fine(:,3)
-       enddo 
-      endif
-         uwnd_buf(:,:,k)=uwnd_fine
-         vwnd_buf(:,:,k)=vwnd_fine
+            if (n_south.eq.-1) then
+              do j=1,2
+                uwnd_fine(:,j)=uwnd_fine(:,3)
+                vwnd_fine(:,j)=vwnd_fine(:,3)
+              enddo 
+            endif
+            uwnd_buf(:,:,k)=uwnd_fine
+            vwnd_buf(:,:,k)=vwnd_fine
  
-       enddo ! k=1,wn
+          enddo ! k=1,wn
 
-       else
+        else
 
           uwnd_buf = reshape(uwnd_buf_coarse, shape(uwnd_buf))    
           vwnd_buf = reshape(vwnd_buf_coarse, shape(vwnd_buf))  
   
-       endif !  if(calc_interp) then !fhx:interp_flag
+        endif !  if(calc_interp) then !fhx:interp_flag
 
-            uwnd_b = uwnd_buf( :, :, 1 )
-            vwnd_b = vwnd_buf( :, :, 1 )
-
-         endif
+        uwnd_b = uwnd_buf( :, :, 1 )
+        vwnd_b = vwnd_buf( :, :, 1 )
 
       endif   !if ( calc_wind ) then
 
@@ -299,95 +269,66 @@
       if (  sec_in_day >= nsec(wn) .and.
      $      sec_in_day - nsec(wn) < int ( dti )  ) then
 
-            uwnd_a = uwnd_buf( :, :, wn )
-            vwnd_a = vwnd_buf( :, :, wn )
+        uwnd_a = uwnd_buf( :, :, wn )
+        vwnd_a = vwnd_buf( :, :, wn )
          
-         d_fwd = d_in + 24 * 3600
-         n = int((d_fwd-d_off)/86400.)*4+1
-         n = mod(n, 1460 + 4*inc_leap(d_in%year))
+        d_fwd = d_in + 24 * 3600
+        n = int((d_fwd-d_off)/86400.)*4+1
+        n = mod(n, 1460 + 4*inc_leap(d_in%year))
 
-!!*      write( infile, '( //trim(windf)//"_",i4.4,2i2.2,".nc" )' )
-!!*  $        d_in%year, d_in%month, d_in%day
-!         write( infile, '( a4,"_",i4.4,2i2.2,".nc" )' )
-!debug     $        windf, d_in%year, d_in%month, d_in%day
-!     $        windf, d_off%year, d_off%month, d_off%day
-         if (calc_mflx) then
-           write( infile, '( a3,".",i4.4,".nc" )' )
-     $        "hfl", d_fwd%year
-         else
-           write( infile, '( a3,".",i4.4,".nc" )' )
-     $        "mfl", d_fwd%year
-         end if
-!!       write( infile, '( "gfsw_",i4.4,2i2.2,".nc" )' )   ! fhx:read gfsw wind
-!!   $        d_in%year, d_in%month, d_in%day
-!         write( infile, '( "gfs_",i4.4,2i2.2,".nc" )' )  ! fhx:read gfs wind
-!     $        d_off%year, d_off%month, d_off%day
+        write( infile, '( i4.4,".nc" )' ) d_fwd%year
 
-! fhx: check wind data exists,is ~exist, set wind to be 0.10/26/2010
-         inquire(file='in/'//trim(windf)//'/'//trim(infile),
-     $           exist=lexist)   
-!!       inquire(file='in/gfsw/'//trim(infile),exist=lexist)   
+        if ( calc_mflx ) then
 
-         if(lexist) then
-
-           if (calc_mflx) then
-             call read_wind_pnetcdfc
+          call read_wind_pnetcdfc
      &        ( uwnd_buf_coarse, vwnd_buf_coarse, trim(infile), n )
-           else
-             call read_mflx_pnetcdf
-     $        ( uwnd_buf_coarse, vwnd_buf_coarse, trim(infile), n )
-           end if
 
-           uwnd_buf_coarse = sf_wi*uwnd_buf_coarse
-           vwnd_buf_coarse = sf_wi*vwnd_buf_coarse
+        else
 
-         else
-            if ( my_task == master_task ) then
-                 write(*,'(/2a)') 
-     $              "missing wind data at wind_main : "
-     $              , trim(infile)
-             endif
-             uwnd_buf_coarse = 0.
-             vwnd_buf_coarse = 0.
-             
-         endif
+          call read_mflx_pnetcdf
+     &        ( uwnd_buf_coarse, vwnd_buf_coarse, trim(infile), n )
+
+        end if
+
+        uwnd_buf_coarse = sf_wi*uwnd_buf_coarse
+        vwnd_buf_coarse = sf_wi*vwnd_buf_coarse
 
 ! interpolation
-      if(calc_interp) then ! fhx:interp_flag:add flag for interp fgrid.  
+        if ( calc_interp ) then ! fhx:interp_flag:add flag for interp fgrid.  
 
-       do k=1,4
-        uwnd_coarse = uwnd_buf_coarse( :, :, k )
-        vwnd_coarse = vwnd_buf_coarse( :, :, k )
-        call interp_mask_2d(uwnd_coarse,1,east_e,north_e,uwnd_fine)
-        call interp_mask_2d(vwnd_coarse,2,east_e,north_e,vwnd_fine)
+          do k=1,4
+            uwnd_coarse = uwnd_buf_coarse( :, :, k )
+            vwnd_coarse = vwnd_buf_coarse( :, :, k )
+            call interp_mask_2d(uwnd_coarse,1,east_e,north_e,uwnd_fine)
+            call interp_mask_2d(vwnd_coarse,2,east_e,north_e,vwnd_fine)
 !fhx: after interpolation, i=1,2 and j=1,2 seem to have problems
-      if (n_west.eq.-1) then
-       do i=1,2
-         uwnd_fine(i,:)=uwnd_fine(3,:)
-         vwnd_fine(i,:)=vwnd_fine(3,:)
-       enddo
-      endif
+            if (n_west.eq.-1) then
+              do i=1,2
+                uwnd_fine(i,:)=uwnd_fine(3,:)
+                vwnd_fine(i,:)=vwnd_fine(3,:)
+              enddo
+            endif
 
-      if(n_south.eq.-1) then
-       do j=1,2
-         uwnd_fine(:,j)=uwnd_fine(:,3)
-         vwnd_fine(:,j)=vwnd_fine(:,3)
-       enddo 
-      endif
-         uwnd_buf(:,:,k)=uwnd_fine
-         vwnd_buf(:,:,k)=vwnd_fine
+            if(n_south.eq.-1) then
+              do j=1,2
+                uwnd_fine(:,j)=uwnd_fine(:,3)
+                vwnd_fine(:,j)=vwnd_fine(:,3)
+              enddo 
+            endif
+            uwnd_buf(:,:,k)=uwnd_fine
+            vwnd_buf(:,:,k)=vwnd_fine
  
-       enddo ! k=1,4 
+          enddo ! k=1,4 
 
-       else
+        else
 
           uwnd_buf = reshape(uwnd_buf_coarse, shape(uwnd_buf))
           vwnd_buf = reshape(vwnd_buf_coarse, shape(vwnd_buf))
   
-       endif !  if(calc_interp) then fhx:interp_flag  
+        endif !  if(calc_interp) then fhx:interp_flag  
          
-           uwnd_b = uwnd_buf( :, :, 1 )
-           vwnd_b = vwnd_buf( :, :, 1 )
+        uwnd_b = uwnd_buf( :, :, 1 )
+        vwnd_b = vwnd_buf( :, :, 1 )
 
       endif
 
@@ -402,15 +343,15 @@
 !       vwsrf = vwind_surf
 
       do j=1,jm
-         do i=1,im
+        do i=1,im
 
-            uwnd = ( 1.0 - aa ) * uwnd_a( i, j ) + aa * uwnd_b( i, j )
-            vwnd = ( 1.0 - aa ) * vwnd_a( i, j ) + aa * vwnd_b( i, j )
+          uwnd = ( 1.0 - aa ) * uwnd_a( i, j ) + aa * uwnd_b( i, j )
+          vwnd = ( 1.0 - aa ) * vwnd_a( i, j ) + aa * vwnd_b( i, j )
 
 !lyo:pac10:exp016:reduce wind to zero west of 129
 !           rdisp=0.5*(1.+tanh((east_e(i,j)-129.0)*0.5))
 !           uwnd=uwnd*rdisp; vwnd=vwnd*rdisp
-          if (calc_mflx) then
+          if ( calc_mflx ) then
 !lyo:pac10:more efficient:
             uwsrf(i,j)=uwnd; vwsrf(i,j)=vwnd;
 
